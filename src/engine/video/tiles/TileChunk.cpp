7369d1e920473f85abaeab5f42d10e04361d5828
bison <bisonmontana@gmail.com>
2017-12-26 17:35:44 +0100

    chunk rendering test

--- src/engine/video/tiles/TileChunk.cpp
+++ src/engine/video/tiles/TileChunk.cpp
@@ -53,7 +53,7 @@ void TileChunk::rebuild() {
         {
             m = glm::translate(m, glm::vec3(1.0f, 0.0f, 0.0f));
             auto pos = m * origin;
-            pos.y = ((float) rand()/ (float) RAND_MAX) / 4.0f;
+            //pos.y = ((float) rand()/ (float) RAND_MAX) / 4.0f;
             auto type = tiles[y*16+x];
             auto tile = tileTypeDict.getTileTypeAt(type);
 
